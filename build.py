4c070f5e4a9801bdc03c650e7e36b38d915b832a
Brian Szmyd <bszmyd@ebay.com>
2018-06-02 20:48:25 +0000

    Fix build args.

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args="--build missing")
+    builder = ConanMultiPackager(args=("--build missing",))
     builder.add_common_builds()
     builder.run()
 ConanMultiPackager
